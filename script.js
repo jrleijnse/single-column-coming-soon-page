630ba9e759a0c0feb6c3e0c7779e5c5786a003d1
Jeroen Leijnse <jr_leijnse@live.nl>
2023-01-22 16:55:17 +0100

    update queries

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const submitEmailBtn = document.querySelector(".btn");
+const notifyContainer = document.querySelector(".notify-container");
+const mobile = window.matchMedia("(max-width: 375px)");
 
 const submitEmail = submitEmailBtn.addEventListener("click", function (e) {
   const inputEmail = document.querySelector(".input");
@@ -11,6 +13,12 @@ const submitEmail = submitEmailBtn.addEventListener("click", function (e) {
   } else {
     !inputEmail.value.includes("@") || inputEmail.length > 0;
     error.textContent = "Please provide a valid email address";
+  }
+
+  // Mobile
+  if (mobile.matches) {
+    error.classList.add("error-mobile");
+    notifyContainer.classList.add("mb");
   }
 
   inputEmail.focus();
