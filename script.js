2b99092aa0a82a9dda283d388041381a7d59f136
Jeroen Leijnse <jr_leijnse@live.nl>
2023-01-22 12:39:42 +0100

    update email validation

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ const submitEmailBtn = document.querySelector(".btn");
 const submitEmail = submitEmailBtn.addEventListener("click", function (e) {
   const inputEmail = document.querySelector(".input");
   const error = document.querySelector(".error");
+
   e.preventDefault();
 
-  if (inputEmail.value === "" || !inputEmail.value.includes("@")) {
-    inputEmail.focus();
-    error.textContent = "Please provide a valid email address";
+  if (inputEmail.value === "") {
+    error.textContent = "Whoops! It looks like you forgot to add your email";
   } else {
-    error.textContent = "";
+    !inputEmail.value.includes("@") || inputEmail.length > 0;
+    error.textContent = "Please provide a valid email address";
   }
+
+  inputEmail.focus();
 });
